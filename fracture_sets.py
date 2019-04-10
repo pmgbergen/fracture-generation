a02b4806ba1dcc3e43801660b11ee3e522b594ed
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-04-10 16:04:25 +0200

    change the importing statement in a module

--- fracture_sets.py
+++ fracture_sets.py
@@ -11,7 +11,7 @@ import scipy.stats as stats
 import logging
 import pdb
 
-from fracture_generation import fracture_network_analysis, distributions
+import fracture_network_analysis, distributions
 import porepy as pp
 
 
